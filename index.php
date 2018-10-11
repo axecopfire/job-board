5ce470c157f5e69a056374dfe8652be36faf6161
axecopfire <braun.sn11@gmail.com>
2018-10-11 13:19:20 -0400

    working around

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include "header.php" ?>
+  <h1>Hello World</h1>
+  <a href="login.php" role="button" >login</a>
+
+
+<?php include "footer.php" ?>
---
+++ login.php
@@ -0,0 +1,17 @@
+
+<?php include  "view/header.php" ?>
+  <form action="" >
+    <label>
+      Username:
+      <input type="text" name="username" >
+    </label>
+    <label>
+      Password
+      <input type="text" name="password" >
+    </label>
+    <input type="submit" value="submit" >
+
+
+  </form>
+
+<?php include "view/footer.php" ?>
